91fb3ce1ea5566bc0afc7f0731032546ad71ef9f
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2022-07-22 00:22:11 +0200

    added print to user_agent.py to test the pipeline

--- user_agent.py
+++ user_agent.py
@@ -1,5 +1,6 @@
 import requests
 print("Welcome to 2022")
+print("Welcome to the world of CI/CD")
 headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
 
 #Lets test what headers are sent by sending a request to HTTPBin
