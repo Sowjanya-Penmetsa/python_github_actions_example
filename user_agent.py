78ee5230a9433e20b60d701703b4c0291b6e9efe
Sowjanya-Penmetsa <penmetsa.sowjanya30@gmail.com>
2022-07-21 22:20:25 +0200

    initial commit

---
+++ user_agent.py
@@ -0,0 +1,8 @@
+import requests
+print("Welcome to 2022")
+headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+
+#Lets test what headers are sent by sending a request to HTTPBin
+r1 = requests.get('http://httpbin.org/headers',headers = headers)
+user_details = r1.text
+print(user_details)
