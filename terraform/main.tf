6f0c6676c1c4ab2449f57ef083ba62363af84742
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2022-08-09 00:44:52 +0200

    modified main.tf to create web_app using tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,38 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-hello-azure-tf"
-    storage_account_name = "helloazuretfstrg"
-    container_name       = "terraform-state"
+    resource_group_name  = var.RG_Name
+    storage_account_name = var.Strg_Acc_Name
+    container_name       = var.Strg_Acc_Container_Name
     key                  = "terraform.tfstate"
   }
 }
 
-resource "azurerm_resource_group" "rg-hello-azure" {
-  name     = "rg-hello-azure"
-  location = "northcentralus"
+resource "azurerm_resource_group" "web_app_rg" {
+  name     = "web_app_rg"
+  location = var.Strg_Acc_Location
   
+}
+
+resource "azurerm_app_service_plan" "web_app_service_plan" {  
+  name                = "web_app_service_plan"  
+  location            = azurerm_resource_group.web_app_rg.location  
+  resource_group_name = azurerm_resource_group.web_app_rg.name  
+  
+  sku {  
+    tier = "Standard"  
+    size = "S1"  
+  }  
+}  
+  
+resource "azurerm_app_service" "web_app_service" {  
+  name                = "web_app_service"  
+  location            =  azurerm_resource_group.web_app_rg.location  
+  resource_group_name = azurerm_resource_group.web_app_rg.name 
+  app_service_plan_id = azurerm_app_service_plan.web_app_service_plan.id  
+  
+  app_settings = {  
+    "DeviceName" = "SampleDevice",  
+    "DeviceId" = "2"  
+  }  
 }
