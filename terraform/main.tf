50a7423796ac81dea2b385c9737a32f7b152d3a2
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2022-08-08 15:24:17 +0200

    Update main.tf
    Added the providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,11 @@
 terraform {
+   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+  
   backend "azurerm" {
     resource_group_name  = "tfrg"
     storage_account_name = "tfstrg30"
