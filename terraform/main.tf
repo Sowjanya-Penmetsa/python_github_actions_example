d6b961c60ab6549046c5532927c9b804712f2180
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2022-08-08 16:37:42 +0200

    removed the added providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,5 @@
 terraform {
-   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-  
+   
   backend "azurerm" {
     resource_group_name  = "tfrg"
     storage_account_name = "tfstrg39"
