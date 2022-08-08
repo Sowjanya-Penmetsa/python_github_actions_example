d983820daaaea5366d60a073ec454d6a7239c1a8
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2022-08-09 00:09:41 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,19 @@
+variable "RG_Name" {
+  type = string
+  default = "strg_rg"
+}
 
+variable "Strg_Acc_Name" {
+  type = string
+  default = "strg_acc_2022"
+}
+
+variable "Strg_Acc_Container_Name" {
+  type = string
+  default = "container_2022"
+}
+
+variable "Strg_Acc_Location" {
+  type = string
+  default = "northcentralus"
+}
