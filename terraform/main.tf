cb511daf30328001591c6221ff34fff80262b4bc
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2022-08-09 14:31:39 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,8 +35,5 @@ resource "azurerm_app_service" "web_app_service" {
   resource_group_name = azurerm_resource_group.web_app_rg.name 
   app_service_plan_id = azurerm_app_service_plan.web_app_service_plan.id  
   
- # app_settings = {  
-  #  "DeviceName" = "SampleDevice",  
-   # "DeviceId" = "2"  
-  #}  
+
 }
