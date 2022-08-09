834d17bde09730d602803557dffa122c5bce0fc7
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2022-08-09 14:47:04 +0200

    changed storage account name

--- terraform/storage/tfstate_strg.tf
+++ terraform/storage/tfstate_strg.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "tfstate_rg" {
 }
 
 resource "azurerm_storage_account" "tfstate_strg_acc" {
-  name                     = "tf_state_str_acc"
+  name                     = "tfstatestracc"
   resource_group_name      = azurerm_resource_group.tfstate_rg.name
   location                 = azurerm_resource_group.tfstate_rg.location
   account_tier             = "Standard"
